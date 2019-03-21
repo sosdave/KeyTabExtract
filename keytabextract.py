4e53fe3b3ee08d16520850cc604994e3cc6e2364
David Storie <dave@seeonesecurity.com>
2019-03-20 20:20:33 -0400

    Updated code to be able to extract NTLM hashes when other encryption types exist

--- keytabextract.py
+++ keytabextract.py
@@ -1,22 +1,23 @@
 #!/usr/local/bin/python3
 import binascii,sys
 
-## TO DO
-# Figure out KeyTypes
-# Figure out TypeNames
-
 # Take argument 1 as keytab file
 ktfile = sys.argv[1]
+f = open(ktfile, 'rb').read()
+hex_encoded = binascii.hexlify(f).decode('utf-8')
 
 def displayhelp():
-    print("KeyTabExtract.")
+    print("KeyTabExtract. Extract NTLM Hashes from KeyTab files where RC4-HMAC encryption has been used.")
     print("Usage : ./keytabextract.py [keytabfile]")
     print("Example : ./keytabextract.py service.keytab")
 
 # Import the keytab file as a hex-encoded string
 def ktextract():
-    f = open(ktfile, 'rb').read()
-    hex_encoded = binascii.hexlify(f).decode('utf-8')
+    if '00170010' in hex_encoded:
+        print("[*] It appears as though RC4-HMAC encryption has been used. Attempting Extraction...")
+    else:
+        print("[!] No NTLM Hash was discovered. This is usually because there is no rc4-hmac encryption type in the keytab file.\n[!] Exiting...")
+        sys.exit()
 
     # First 16 bits are dedicated to stating the version of Keytab File
     ktversion = hex_encoded[:4]
@@ -39,7 +40,7 @@ def ktextract():
 
     # Determine the realm for the keytab file
     realm = hex_encoded[20:realm_jump]
-    print("     REALM : " + bytes.fromhex(realm).decode('utf-8'))
+    print("\tREALM : " + bytes.fromhex(realm).decode('utf-8'))
 
     # Calculate the number of bytes for the realm of components
     comp_array_calc  = realm_jump + 4
@@ -58,27 +59,32 @@ def ktextract():
     prin_array_start = principal_array_offset
     prin_array_finish = prin_array_start + principal_array_int
     principal_array_value = hex_encoded[prin_array_start:prin_array_finish]
-    print("     SERVICE PRINCIPAL : " + bytes.fromhex(comp_array2).decode('utf-8') + "/" + bytes.fromhex(principal_array_value).decode('utf-8'))
+    print("\tSERVICE PRINCIPAL : " + bytes.fromhex(comp_array2).decode('utf-8') + "/" + bytes.fromhex(principal_array_value).decode('utf-8'))
 
     # Calculate typename - 32 bits from previous value
     typename_offset = prin_array_finish + 8
     typename = hex_encoded[prin_array_finish:typename_offset]
-    print("     TYPENAME : " + typename)
+    #print("\tTYPENAME : " + typename)
 
     # Calculate Timestamp - 32 bit from typename value
     timestamp_offset = typename_offset + 8
     timestamp = hex_encoded[typename_offset:timestamp_offset]
-    print("     TIMESTAMP : " + timestamp)
+    #print("\tTIMESTAMP : " + timestamp)
 
     # Calcualte 8 bit VNO Field
     vno_offset = timestamp_offset + 2
     vno = hex_encoded[timestamp_offset:vno_offset]
-    print("     VNO : " + vno)
+    #print("\tVersion No : " + vno)
 
     # Calculate KeyType - 16 bit value
     keytype_offset = vno_offset + 4
-    keytype = hex_encoded[vno_offset:keytype_offset]
-    print("     KEYTYPE : " + keytype)
+    keytype_hex = hex_encoded[vno_offset:keytype_offset]
+    keytype_dec = int(keytype_hex, 16)
+    if keytype_dec == '23':
+        encType = 'rc4-hmac'
+    else:
+        encType = 'Unknown'
+    print("\tENCRYPTION TYPE: 0017 (rc4-hmac)")
 
     # Calculate Length of Key Value - 16 bit value
     key_val_offset = keytype_offset + 4
@@ -89,12 +95,12 @@ def ktextract():
     key_val_start = key_val_offset
     key_val_finish = key_val_start + (key_val_len * 2)
     key_val = hex_encoded[key_val_start:key_val_finish]
-    print("     NTLM HASH : " + key_val)
+    NTLMHash = hex_encoded.split("00170010")[1]
+    print("\tNTLM HASH : " + NTLMHash[:32])
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         displayhelp()
         sys.exit()
     else:
-        print("[*] KeyTab Extraction Started.")
         ktextract()
