fec6c762f36548b1322000714db2aae207a24961
David Storie <dave@seeonesecurity.com>
2019-04-07 12:03:28 -0400

    Added support for AES128-CTS-HMAC-SHA1 and AES256-CTS-HMAC-SHA1 hash types

--- keytabextract.py
+++ keytabextract.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 import binascii,sys
 
-# Take argument 1 as keytab file
+# Take argument 1 as keytab file, import and decode the hex
 ktfile = sys.argv[1]
 f = open(ktfile, 'rb').read()
 hex_encoded = binascii.hexlify(f).decode('utf-8')
@@ -11,13 +11,33 @@ def displayhelp():
     print("Usage : ./keytabextract.py [keytabfile]")
     print("Example : ./keytabextract.py service.keytab")
 
-# Import the keytab file as a hex-encoded string
 def ktextract():
+    rc4hmac = False
+    aes128 = False
+    aes256 = False 
+    
     if '00170010' in hex_encoded:
-        print("[*] It appears as though RC4-HMAC encryption has been used. Attempting Extraction...")
+        print("[*] RC4-HMAC Encryption detected. Will attempt to extract NTLM hash.")
+        rc4hmac = True
     else:
-        print("[!] No NTLM Hash was discovered. This is usually because there is no rc4-hmac encryption type in the keytab file.\n[!] Exiting...")
-        sys.exit()
+        print("[!] No RC4-HMAC located. Unable to extract NTLM hashes.")
+        
+    if '00120020' in hex_encoded:
+        print("[*] AES256-CTS-HMAC-SHA1 key found. Will attempt hash extraction.")
+        aes256 = True
+    else:
+        print("[!] Unable to identify any AES256-CTS-HMAC-SHA1 hashes.")
+
+    if '00110010' in hex_encoded:
+        print("[*] AES128-CTS-HMAC-SHA1 hash discovered. Will attempt hash extraction.")
+        aes128 = True
+    else:
+        print("[!] Unable to identify any AES128-CTS-HMAC-SHA1 hashes.")
+
+    #if proceed != True:
+    if all( [ rc4hmac != True, aes256 != True, aes128 != True]):
+        print("Unable to find any useful hashes.\nExiting...")
+        sys.exit
 
     # First 16 bits are dedicated to stating the version of Keytab File
     ktversion = hex_encoded[:4]
@@ -64,12 +84,10 @@ def ktextract():
     # Calculate typename - 32 bits from previous value
     typename_offset = prin_array_finish + 8
     typename = hex_encoded[prin_array_finish:typename_offset]
-    #print("\tTYPENAME : " + typename)
 
     # Calculate Timestamp - 32 bit from typename value
     timestamp_offset = typename_offset + 8
     timestamp = hex_encoded[typename_offset:timestamp_offset]
-    #print("\tTIMESTAMP : " + timestamp)
 
     # Calcualte 8 bit VNO Field
     vno_offset = timestamp_offset + 2
@@ -80,23 +98,26 @@ def ktextract():
     keytype_offset = vno_offset + 4
     keytype_hex = hex_encoded[vno_offset:keytype_offset]
     keytype_dec = int(keytype_hex, 16)
-    if keytype_dec == '23':
-        encType = 'rc4-hmac'
-    else:
-        encType = 'Unknown'
-    print("\tENCRYPTION TYPE: 0017 (rc4-hmac)")
 
     # Calculate Length of Key Value - 16 bit value
     key_val_offset = keytype_offset + 4
     key_val_len = int(hex_encoded[keytype_offset:key_val_offset], 16)
-    #print(key_val_len)
 
     # Extract Key Value
     key_val_start = key_val_offset
     key_val_finish = key_val_start + (key_val_len * 2)
     key_val = hex_encoded[key_val_start:key_val_finish]
-    NTLMHash = hex_encoded.split("00170010")[1]
-    print("\tNTLM HASH : " + NTLMHash[:32])
+    if rc4hmac == True:
+        NTLMHash = hex_encoded.split("00170010")[1]
+        print("\tNTLM HASH : " + NTLMHash[:32])
+
+    if aes256 == True:
+        aes256hash = hex_encoded.split("00120020")[1]
+        print("\tAES-256 HASH : " + aes256hash[:64])
+
+    if aes128 == True:
+        aes128hash = hex_encoded.split("00110010")[1]
+        print("\tAES-128 HASH : " + aes128hash[:32])
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
